438704e37f91008cfff7331ed23003f43f35e488
Tom Longson (nym) <tomlong@gmail.com>
2016-05-05 13:01:04 -0700

    Removing book cruft from original example.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,10 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducer_books';
 import ProductsReducer from './reducer_products';
 import YearsReducer from './reducer_years';
-import ActiveBook from './reducer_active_book';
 import ActiveProduct from './reducer_active_product';
 import ActiveYear from './reducer_active_year';
 
 const rootReducer = combineReducers({
-	books: BooksReducer,
-	activeBook: ActiveBook,
 	years: YearsReducer,
 	products: ProductsReducer,
 	activeProduct: ActiveProduct,
